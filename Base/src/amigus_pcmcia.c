a06cb3877349545319a714d4db7050264b5d9bdb
Christoph Fassbach <christoph.fassbach@gmail.com>
2026-01-12 12:31:22 +0100

    Base: Add AmiGUS PCMCIA / mini stubs

---
+++ Base/src/amigus_pcmcia.c
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the amigus.library.
+ *
+ * amigus.library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License only.
+ *
+ * amigus.library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with amigus.library.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <amigus/amigus.h>
+
+#include "amigus_pcmcia.h"
+#include "errors.h"
+
+VOID AmiGusPcmcia_AddAll( struct List * cards ) {
+
+  return;
+}
+
+VOID AmiGusPcmcia_InstallInterrupt( VOID ) {
+
+  return;
+}
+
+VOID AmiGusPcmcia_RemoveInterrupt( VOID ) {
+
+  return;
+}
